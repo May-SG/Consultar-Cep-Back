2f9b1628c987d6064c0e90731d5a4308b47adbd2
Mayara Gomes <aikyih.gomes@gmail.com>
2026-03-19 19:26:51 -0300

    Adição de tratamento de erro e alteração no RestService

--- src/main/java/com/consultar/cep/service/ViaCepRestService.java
+++ src/main/java/com/consultar/cep/service/ViaCepRestService.java
@@ -1,5 +1,6 @@
 package com.consultar.cep.service;
 
+import com.consultar.cep.exceptions.ResourceNotFoundException;
 import com.consultar.cep.model.Endereco;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
@@ -7,6 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class ViaCepRestService {
@@ -28,12 +32,15 @@ public class ViaCepRestService {
                         url,
                         Endereco.class
                 );
-        if (response.getStatusCode() == HttpStatus.OK &&
-        response.getBody() != null) {
-            return response.getBody();
+        if (response.getStatusCode() == HttpStatus.OK
+                && response.getBody() != null) {
+            Endereco endereco = response.getBody();
+            if (endereco.isErro()) {
+                throw new ResourceNotFoundException("CEP " + cep + " não encontrado pelo ViaCEP.");
+            }
+            return endereco;
         } else {
-            throw new RuntimeException(
-                    "CEP não encontrado ou erro na consulta");
+            throw new ResourceNotFoundException("CEP não encontrado ou erro na consulta");
         }
     }
 
